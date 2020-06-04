264df89f12e6bff78f6c44028a522a0e9b4d01ab
mohiu <mohiuddintarek2017@gmail.com>
2020-06-04 21:13:15 +0600

    home done

--- Mshop/app/src/main/java/com/techtrickbd/nshop/activities/HomeActivity.java
+++ Mshop/app/src/main/java/com/techtrickbd/nshop/activities/HomeActivity.java
@@ -1,16 +1,69 @@
 package com.techtrickbd.nshop.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.techtrickbd.nshop.R;
+import com.techtrickbd.nshop.fragments.HomeFragment;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
-    
+    DrawerLayout drawerLayout;
+    private CircleImageView imageView;
+    private TextView username, number;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView nav_view = findViewById(R.id.nav_view);
+        View headerView = nav_view.getHeaderView(0);
+        imageView = headerView.findViewById(R.id.profiel_image_id);
+        username = headerView.findViewById(R.id.profile_users_name);
+        number = headerView.findViewById(R.id.profile_email_id);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        nav_view.setCheckedItem(R.id.home_id);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+                    case R.id.home_id:
+
+                        break;
+                    case R.id.profile_id:
+
+                        break;
+                    /*case R.id.close:
+                        finish();
+                        break;*/
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
     }
 }
